184324b187bb4b6df44a7838c601fed240758e87
shenpengfei <shenpengfei1219@vip.163.com>
2024-08-10 11:33:33 +0800

    0.1.0

--- ios/Classes/AzureSdkVoicePlugin.swift
+++ ios/Classes/AzureSdkVoicePlugin.swift
@@ -2,16 +2,17 @@ import Flutter
 import UIKit
 
 public class AzureSdkVoicePlugin: NSObject, FlutterPlugin {
+    static var gChannel: FlutterMethodChannel?;
+    
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "azure_sdk_voice", binaryMessenger: registrar.messenger())
+      gChannel = channel
     let instance = AzureSdkVoicePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   var _audioRecorder: AudioRecorder = AudioRecorder();
-
-  var key: String! = ""
-  var region: String! = ""
+  var _azureSdk: AzureSdk = AzureSdk();
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
@@ -22,8 +23,7 @@ public class AzureSdkVoicePlugin: NSObject, FlutterPlugin {
           result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments", details: nil))
           return
       }
-      key = keyTmp;
-      region = regionTmp;
+        _azureSdk.initKeyRegion(key: keyTmp, region: regionTmp)
       result("OK")
     case "startRecording":
       guard let arguments = call.arguments as? [String: Any],
@@ -52,7 +52,7 @@ public class AzureSdkVoicePlugin: NSObject, FlutterPlugin {
           result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments", details: nil))
           return
       }
-      pronunciationAssessmentWithContentAssessment(key: key,region: region,fileName: nameTmp,language: languageTmp,topic: topicTmp) {res in
+        _azureSdk.pronunciationAssessmentWithContentAssessment(fileName: nameTmp,language: languageTmp,topic: topicTmp) {res in
         result(res)
       }
     case "translate":
@@ -63,20 +63,52 @@ public class AzureSdkVoicePlugin: NSObject, FlutterPlugin {
           result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments", details: nil))
           return
       }
-      translateWav(key: key,region: region,fileName: nameTmp,recLanguage: recLanguageTmp,toLanguage: toLanguageTmp) {res in
+        _azureSdk.translateWav(fileName: nameTmp,recLanguage: recLanguageTmp,toLanguage: toLanguageTmp) {res in
         result(res)
       }
+    case "startTranslateContinuous":
+        guard let arguments = call.arguments as? [String: Any],
+              let recLanguageTmp = arguments["recLanguage"] as? String,
+              let toLanguageTmp = arguments["toLanguage"] as? String,
+              let code = arguments["code"] as? Int else {
+            result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments", details: nil))
+            return
+        }
+        _azureSdk.startTranslateContinuous(recLanguage: recLanguageTmp,toLanguage: toLanguageTmp) {res,is_last in
+            DispatchQueue.global(qos: .userInitiated).async {
+                let arg2:[String:Any] = ["code":code,"res": res!,"is_last":is_last!]
+                AzureSdkVoicePlugin.gChannel?.invokeMethod("callback", arguments: arg2)
+            }
+        }
+        result("OK")
+    case "stopTranslateContinuous":
+        _azureSdk.stopTranslateContinuous()
+        result("OK")
     case "speak":
       guard let arguments = call.arguments as? [String: Any],
+            let code = arguments["code"] as? Int,
             let contentTmp = arguments["content"] as? String else {
           result(FlutterError(code: "INVALID_ARGUMENTS", message: "Invalid arguments", details: nil))
           return
       }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let arg2:[String:Any] = ["code":code,"res": "start","is_last":false]
+            AzureSdkVoicePlugin.gChannel?.invokeMethod("callback", arguments: arg2)
+        
+        
+            self._azureSdk.synthesisToSpeaker(inputText: contentTmp)
+        
 
-      DispatchQueue.global(qos: .userInitiated).async {
-                  synthesisToSpeaker(key: self.key,region: self.region,inputText: contentTmp)
-              }
-      result("OK")
+            let arg3:[String:Any] = ["code":code,"res": "stop","is_last":true]
+            AzureSdkVoicePlugin.gChannel?.invokeMethod("callback", arguments: arg3)
+        }
+        
+        
+        result("OK")
+    case "speak_stop":
+        _azureSdk.stopSynthesisToSpeaker()
+        result("OK")
     default:
       result(FlutterMethodNotImplemented)
     }
